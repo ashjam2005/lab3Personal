94af8ba2eefa642e1bbba694ffb824e9b30192ab
ashjam2005 <122492177+ashjam2005@users.noreply.github.com>
2023-01-25 14:24:50 -0800

    attempt to commit and push

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,21 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testAnotherTypeOfReverse()
+  {
+    
+    int[] input1 = {23,34,45,67};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{67,45,34,23},input1);
+  }
+
+  @Test
+  public void testSecondTypeOfReverse()
+  {
+    int[] input1 = {23,34,45};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{45,34,23},input1);
+  }
 }
